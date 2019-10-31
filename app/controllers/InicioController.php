f06554a434f3f5cf0fb094e0aa725a004bdc70f3
paolaflores10 <42444073+paolaflores10@users.noreply.github.com>
2019-10-31 15:50:39 -0400

    Cambio

--- app/controllers/InicioController.php
+++ app/controllers/InicioController.php
@@ -2,11 +2,9 @@
 class InicioController extends BaseController{
 	//FUNCION QUE GENERA LA PAGINA DE INICIO
 	public function Inicio(){
-
-		//$title = Lang::choice('translate.inicio_tit',Session::get('nivel'));
 		
 		return View::make('inicio.inicio', array(
-			//'title' => $title
+
 		));
 	}	
 }
